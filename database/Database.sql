cb67ce93922f34c81765fbe32a6727c250681897
Mario Hernandez <9300322@alumnos.ufv.es>
2025-03-24 22:10:53 +0100

    add database

---
+++ database/Database.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE UniGen;
+USE miapp;
+
+CREATE TABLE usuario (
+    idusuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    passwordHash VARCHAR(255)
+);
+
+CREATE TABLE actividad (
+    idactividad INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    fecha DATE,
+    lugar VARCHAR(255),
+    descripcion TEXT,
+    duracion INT
+);
+
+CREATE TABLE participacion (
+    idusuario INT,
+    idactividad INT,
+    puntos INT DEFAULT 0,
+    PRIMARY KEY (idusuario, idactividad),
+    FOREIGN KEY (idusuario) REFERENCES usuario(idusuario),
+    FOREIGN KEY (idactividad) REFERENCES actividad(idactividad)
+);
+
+CREATE TABLE creacion (
+    idusuario INT,
+    idactividad INT,
+    PRIMARY KEY (idusuario, idactividad),
+    FOREIGN KEY (idusuario) REFERENCES usuario(idusuario),
+    FOREIGN KEY (idactividad) REFERENCES actividad(idactividad)
+);
