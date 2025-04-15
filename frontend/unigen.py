b89592c9686e1220bd50e00522f6944f5b9d909c
pazos@gmail.com <151624095+Alex3456787@users.noreply.github.com>
2025-04-15 11:43:14 +0200

    🐛 problemas del merge con el morito

--- frontend/unigen.py
+++ frontend/unigen.py
@@ -23,36 +23,13 @@ def actividad():
 def dashboard():
     return render_template("dashboard.html")
 
-<<<<<<< HEAD
-# Registro: muestra la vista y procesa el registro
-@app.route("/register", methods=["GET", "POST"])
-def register():
-=======
 @app.route("/sobrenosotros.html")
 def sobre_nosotros():
     return render_template("sobrenosotros.html")
 
-@app.route("/login", methods=["POST"])
-def login():
-    # Capturar datos del formulario
-    data = {
-        "username": request.form["username"],
-        "password": request.form["password"]
-    }
-    # Enviar datos a la API
-    response = requests.post(f"{API_BASE_URL}/auth/login", json=data)
-    if response.status_code == 200:
-        usuario = response.json().get("usuario")  # Extrae los datos del usuario
-        return redirect(url_for("dashboard", username=usuario["username"]))
-    elif response.status_code == 401:
-        return "Error: Usuario o contraseña incorrectos", 401
-    else:
-        return f"Error en la conexión con la API: {response.text}", response.status_code
-
 
 @app.route("/registro", methods=["GET", "POST"])
 def registro():
->>>>>>> 30d6e6c43f66ff5ee8b690d1e5b663ee94965d14
     if request.method == "POST":
         # Enviar datos del formulario a la API
         data = {
@@ -61,15 +38,8 @@ def registro():
         }
         response = requests.post(f"{API_BASE_URL}/auth/registro", json=data)
         if response.status_code == 200:
-<<<<<<< HEAD
-            return redirect(url_for("index"))  # Redirige al login después del registro
-        else:
-            return f"Error al registrar: {response.text}", response.status_code
-    return render_template("register.html")
-=======
             return redirect(url_for("login.html"))
     return render_template("registro.html")
->>>>>>> 30d6e6c43f66ff5ee8b690d1e5b663ee94965d14
 
 # Procesa el login
 @app.route("/login", methods=["POST"])
