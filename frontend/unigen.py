5e49c2b4b35921c24606b8b65cdab295edd337da
Mario Hernandez <9300322@alumnos.ufv.es>
2025-05-12 19:10:33 +0200

    update foto chuso

--- frontend/unigen.py
+++ frontend/unigen.py
@@ -10,7 +10,7 @@ app.secret_key = "miclavesecreta123"
 
 API_BASE_URL = "http://localhost:5001"
 UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static", "IMAGES", "perfiles")
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "svg"}
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "svg", "webp"}
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # Página principal: Login
